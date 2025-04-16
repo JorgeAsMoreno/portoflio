51e7042ea5b3439fc7a5423ca9b719d9dad77087
Josom0 <jorge.solis@homie.mx>
2025-04-15 22:28:28 -0600

    button

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -111,7 +111,7 @@ const Hero = () => {
             href='#projects'
             className='inline-block mt-8 px-6 py-3 bg-[#e9c0e9] text-[#292522] text-2xl font-extrabold rounded-xl shadow transition-all hover:bg-[#d2e823] hover:text-[#1e2330] hover:shadow-lg mb-5'
           >
-            See My Work ↓
+            See My Work
           </motion.a>
         </div>
 
